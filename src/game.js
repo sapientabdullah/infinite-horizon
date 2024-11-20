01d2c1112d48b4c3b2ad3c989af784461f8621ed
Abdullah <sapientabdullah@gmail.com>
2024-11-20 18:08:05 +0500

    initial commit: add game files and setup README, .gitignore, HTML, CSS, and JavaScript for Infinite Horizon

---
+++ src/game.js
@@ -0,0 +1,676 @@
+import * as THREE from "three";
+import { OrbitControls } from "jsm/controls/OrbitControls.js";
+import { EffectComposer } from "jsm/postprocessing/EffectComposer.js";
+import { RenderPass } from "jsm/postprocessing/RenderPass.js";
+import { UnrealBloomPass } from "jsm/postprocessing/UnrealBloomPass.js";
+import { GLTFLoader } from "jsm/loaders/GLTFLoader.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  set,
+} from "https://www.gstatic.com/firebasejs/11.0.2/firebase-database.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBDP807efNTMOuQc6jdi23NKnb2LbvxBlM",
+  authDomain: "infinite-horizon-1.firebaseapp.com",
+  projectId: "infinite-horizon-1",
+  storageBucket: "infinite-horizon-1.firebasestorage.app",
+  messagingSenderId: "393334686227",
+  appId: "1:393334686227:web:06ca63ecd18e2c43d2f5e6",
+  measurementId: "G-GDXH9NE5PT",
+};
+
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+
+const scene = new THREE.Scene();
+scene.fog = new THREE.FogExp2(0x000000, 0.04);
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+camera.position.set(0, 2, 0);
+camera.position.z = 5;
+
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.shadowMap.enabled = true;
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.toneMapping = THREE.ACESFilmicToneMapping;
+renderer.outputColorSpace = THREE.SRGBColorSpace;
+document.body.appendChild(renderer.domElement);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+
+const renderScene = new RenderPass(scene, camera);
+const bloomPass = new UnrealBloomPass(
+  new THREE.Vector2(window.innerWidth, window.innerHeight),
+  1.5,
+  0.4,
+  100
+);
+bloomPass.threshold = 0.002;
+bloomPass.strength = 2.0;
+bloomPass.radius = 0;
+const composer = new EffectComposer(renderer);
+composer.addPass(renderScene);
+composer.addPass(bloomPass);
+
+const slowModeOverlay = new THREE.Mesh(
+  new THREE.PlaneGeometry(2, 2),
+  new THREE.ShaderMaterial({
+    uniforms: {
+      uOpacity: { value: 0.0 },
+      uColor: { value: new THREE.Color(0x0369a1) },
+    },
+    vertexShader: `
+      void main() {
+        gl_Position = vec4(position, 1.0);
+      }
+    `,
+    fragmentShader: `
+      uniform float uOpacity;
+      uniform vec3 uColor;
+
+      void main() {
+        gl_FragColor = vec4(uColor, uOpacity);
+      }
+    `,
+    transparent: true,
+  })
+);
+
+slowModeOverlay.frustumCulled = false;
+scene.add(slowModeOverlay);
+
+const enemies = [];
+const speedLines = [];
+let isJumping = false;
+let isGameOver = false;
+let isPaused = false;
+let frames = 0;
+let timeScale = 1.0;
+let spawnRate = 15;
+let cubeVelocity = 0.15;
+let enemyVelocity = 0.3;
+let startTime = Date.now();
+let survivedTime = 0;
+
+class Box extends THREE.Mesh {
+  constructor({
+    width,
+    height,
+    depth,
+    color = 0xffff00,
+    velocity = {
+      x: 0,
+      y: 0,
+      z: 0,
+    },
+    position = {
+      x: 0,
+      y: 0,
+      z: 0,
+    },
+    zAcceleration = false,
+    emissiveIntensity = 1,
+  }) {
+    super(
+      new THREE.BoxGeometry(width, height, depth),
+      new THREE.MeshStandardMaterial({
+        color,
+        emissive: color,
+        emissiveIntensity,
+      })
+    );
+    this.height = height;
+    this.width = width;
+    this.depth = depth;
+    this.position.set(position.x, position.y, position.z);
+
+    this.right = this.position.x + this.width / 2;
+    this.left = this.position.x - this.width / 2;
+
+    this.bottom = this.position.y - this.height / 2;
+    this.top = this.position.y + this.height / 2;
+
+    this.front = this.position.z + this.depth / 2;
+    this.back = this.position.z - this.depth / 2;
+
+    this.velocity = velocity;
+    this.gravity = -0.002;
+
+    this.zAcceleration = zAcceleration;
+  }
+  updateSides() {
+    this.bottom = this.position.y - this.height / 2;
+    this.top = this.position.y + this.height / 2;
+
+    this.right = this.position.x + this.width / 2;
+    this.left = this.position.x - this.width / 2;
+
+    this.front = this.position.z + this.depth / 2;
+    this.back = this.position.z - this.depth / 2;
+  }
+  update(ground) {
+    this.updateSides();
+    if (this.zAcceleration) {
+      this.velocity.z += 0.0001;
+    }
+    this.position.x += this.velocity.x;
+    this.position.z += this.velocity.z;
+
+    this.applyGravity(ground);
+  }
+
+  applyGravity(ground) {
+    this.velocity.y += this.gravity;
+
+    if (
+      boxCollision({
+        box1: this,
+        box2: ground,
+      })
+    ) {
+      const friction = 0.5;
+      this.velocity.y *= friction;
+      this.velocity.y = -this.velocity.y;
+    } else {
+      this.position.y += this.velocity.y;
+    }
+  }
+}
+
+function boxCollision({ box1, box2 }) {
+  const zCollision = box1.front >= box2.back && box1.back <= box2.front;
+  const yCollision =
+    box1.bottom + box1.velocity.y <= box2.top && box1.top >= box2.bottom;
+  const xCollision = box1.right >= box2.left && box1.left <= box2.right;
+
+  return xCollision && yCollision && zCollision;
+}
+
+const loader = new GLTFLoader();
+function createCubeWithModel(modelUrl, cubeConfig) {
+  const cube = new Box(cubeConfig);
+  loader.load(
+    modelUrl,
+    (gltf) => {
+      const model = gltf.scene;
+
+      model.scale.set(
+        cubeConfig.scale || 0.2,
+        cubeConfig.scale || 0.2,
+        cubeConfig.scale || 0.2
+      );
+
+      model.rotation.y = Math.PI;
+      cube.add(model);
+    },
+    undefined,
+    (error) => {
+      console.error("Error loading GLTF model:", error);
+    }
+  );
+  return cube;
+}
+
+const cubeConfig = {
+  width: 1.4,
+  height: 1,
+  depth: 1.4,
+  position: { x: 0, y: 2, z: 0 },
+  velocity: { x: 0, y: -0.01, z: 0 },
+  zAcceleration: false,
+};
+const cube = createCubeWithModel("../public/spaceship/scene.gltf", cubeConfig);
+cube.material = new THREE.MeshBasicMaterial({
+  opacity: 0,
+  transparent: true,
+});
+cube.castShadow = true;
+scene.add(cube);
+
+const ground = new Box({
+  width: 20,
+  height: 0.5,
+  depth: 100,
+  color: 0x0369a1,
+  position: {
+    x: 0,
+    y: -2,
+    z: 0,
+  },
+});
+ground.material = new THREE.MeshBasicMaterial({
+  color: 0x0369a1,
+  wireframe: true,
+  side: THREE.DoubleSide,
+});
+ground.receiveShadow = true;
+scene.add(ground);
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+directionalLight.position.y = 3;
+directionalLight.position.z = 1;
+directionalLight.castShadow = true;
+scene.add(directionalLight);
+
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+scene.add(ambientLight);
+
+const keys = {
+  a: {
+    pressed: false,
+  },
+  d: {
+    pressed: false,
+  },
+  w: {
+    pressed: false,
+  },
+  s: {
+    pressed: false,
+  },
+};
+
+window.addEventListener("keydown", (event) => {
+  switch (event.code) {
+    case "KeyA":
+      keys.a.pressed = true;
+      break;
+    case "KeyD":
+      keys.d.pressed = true;
+      break;
+    case "KeyW":
+      keys.w.pressed = true;
+      break;
+    case "KeyS":
+      keys.s.pressed = true;
+      break;
+    case "Space":
+      if (!isJumping) {
+        cube.velocity.y = 0.08;
+        isJumping = true;
+      }
+      break;
+    case "ShiftRight":
+      timeScale = 0.3;
+      slowModeOverlay.material.uniforms.uOpacity.value = 0.04;
+      muffleAudio();
+      break;
+  }
+});
+
+window.addEventListener("keyup", (event) => {
+  switch (event.code) {
+    case "KeyA":
+      keys.a.pressed = false;
+      break;
+    case "KeyD":
+      keys.d.pressed = false;
+    case "KeyW":
+      keys.w.pressed = false;
+      break;
+    case "KeyS":
+      keys.s.pressed = false;
+    case "Space":
+      isJumping = false;
+      break;
+    case "ShiftRight":
+      timeScale = 1.0;
+      slowModeOverlay.material.uniforms.uOpacity.value = 0.0;
+      unmuffleAudio();
+      break;
+  }
+});
+
+const starsTextureUrl = "../public/background.jpg";
+const cubeTextureLoader = new THREE.CubeTextureLoader();
+scene.background = cubeTextureLoader.load([
+  starsTextureUrl,
+  starsTextureUrl,
+  starsTextureUrl,
+  starsTextureUrl,
+  starsTextureUrl,
+  starsTextureUrl,
+]);
+
+setInterval(() => {
+  enemyVelocity += 0.02;
+}, 1000);
+
+function enemyPositionGenerator() {
+  return (Math.random() - 0.5) * 20;
+}
+function getRandomColor() {
+  return Math.floor(Math.random() * 16777215);
+}
+
+function createSpeedLines(numLines = 100) {
+  for (let i = 0; i < numLines; i++) {
+    const x = (Math.random() - 0.5) * 100;
+    const y = (Math.random() - 0.5) * 100;
+    const z = Math.random() * -100;
+
+    const lineLength = Math.random() * 10 + 5;
+
+    const geometry = new THREE.BufferGeometry();
+    const positions = new Float32Array(2 * 3);
+
+    positions[0] = x;
+    positions[1] = y;
+    positions[2] = z;
+
+    positions[3] = x;
+    positions[4] = y;
+    positions[5] = z - lineLength;
+
+    geometry.setAttribute("position", new THREE.BufferAttribute(positions, 3));
+
+    const material = new THREE.LineBasicMaterial({
+      color: 0xffffff,
+      opacity: 0.8,
+      transparent: true,
+      linewidth: 2,
+    });
+
+    const line = new THREE.Line(geometry, material);
+    scene.add(line);
+    speedLines.push(line);
+  }
+}
+
+createSpeedLines();
+
+function updateSpeedLines(timeScale = 1) {
+  speedLines.forEach((line) => {
+    const positions = line.geometry.attributes.position.array;
+
+    for (let i = 0; i < positions.length; i += 3) {
+      positions[i + 2] += 0.5 * timeScale;
+      if (positions[i + 2] > 5) {
+        positions[i + 2] = -100;
+      }
+    }
+
+    line.geometry.attributes.position.needsUpdate = true;
+  });
+}
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  bloomPass.setSize(window.innerWidth, window.innerHeight);
+}
+
+function adjustCameraFov() {
+  const fov = window.innerWidth / window.innerHeight > 1.6 ? 75 : 65;
+  camera.fov = fov;
+  camera.updateProjectionMatrix();
+}
+
+window.addEventListener("resize", () => {
+  onWindowResize();
+  adjustCameraFov();
+});
+
+const timeElement = document.getElementById("survival-time");
+timeElement.innerText = "0s";
+document.body.appendChild(timeElement);
+
+const gamePlayPauseBtn = document.getElementById("game-play-pause-btn");
+const playPauseImg = document.getElementById("play-pause-img");
+gamePlayPauseBtn.addEventListener("click", () => {
+  togglePause();
+  gamePlayPauseBtn.blur();
+});
+
+function togglePause() {
+  if (isGameOver) return;
+  if (isPaused) {
+    resumeGame();
+  } else {
+    pauseGame();
+  }
+}
+
+function pauseGame() {
+  isPaused = true;
+  playPauseImg.src = "https://cdn-icons-png.flaticon.com/512/3318/3318660.png";
+  console.log("Game paused");
+  cancelAnimationFrame(animationId);
+}
+
+function resumeGame() {
+  isPaused = false;
+  playPauseImg.src =
+    "https://www.pngarts.com/files/4/Pause-Button-PNG-Free-Download.png";
+  console.log("Game resumed");
+  animate();
+}
+
+window.addEventListener("keydown", (event) => {
+  if (event.code === "KeyP" && !isGameOver) {
+    togglePause();
+    document.activeElement.blur();
+  }
+});
+
+document.getElementById("restart-btn").addEventListener("click", () => {
+  restartGame();
+});
+
+function restartGame() {
+  enemies.forEach((enemy) => {
+    scene.remove(enemy);
+  });
+  enemies.length = 0;
+  survivedTime = 0;
+  timeElement.innerText = "0";
+  frames = 0;
+  enemyVelocity = 0.3;
+  enemies.length = 0;
+
+  cube.position.set(0, 2, 0);
+  cube.velocity = { x: 0, y: 0, z: 0 };
+
+  document.getElementById("game-over-overlay").style.display = "none";
+
+  startTime = Date.now();
+  isGameOver = false;
+  animate();
+}
+
+function animate() {
+  if (isPaused) return;
+  const animationId = requestAnimationFrame(animate);
+
+  cube.velocity.x = 0;
+  cube.velocity.z = 0;
+
+  if (keys.w.pressed) {
+    cube.velocity.z -= cubeVelocity * timeScale;
+  } else if (keys.s.pressed) {
+    cube.velocity.z += cubeVelocity * timeScale;
+  }
+  if (keys.a.pressed) {
+    cube.velocity.x = -cubeVelocity * timeScale;
+  } else if (keys.d.pressed) {
+    cube.velocity.x = cubeVelocity * timeScale;
+  }
+
+  updateSpeedLines(timeScale);
+  cube.update(ground);
+
+  const elapsedTime = (Date.now() - startTime) / 1000;
+  survivedTime = Math.floor(elapsedTime);
+  timeElement.innerText = `${survivedTime}${survivedTime === 0 ? "" : "s"}`;
+
+  camera.position.x = THREE.MathUtils.lerp(
+    camera.position.x,
+    cube.position.x,
+    0.1
+  );
+
+  enemies.forEach((enemy) => {
+    enemy.velocity.z *= timeScale;
+    enemy.update(ground);
+    enemy.velocity.z /= timeScale;
+
+    if (
+      boxCollision({
+        box1: cube,
+        box2: enemy,
+      })
+    ) {
+      cancelAnimationFrame(animationId);
+      isGameOver = true;
+      timeElement.innerText = "";
+      document.getElementById("final-time").innerText = survivedTime;
+      document.getElementById("game-over-overlay").style.display = "flex";
+    }
+  });
+
+  if (frames % spawnRate === 0) {
+    if (spawnRate > 10) {
+      spawnRate -= 5;
+    }
+    const randomColor = getRandomColor();
+    const enemy = new Box({
+      width: 1,
+      height: 1,
+      depth: 1,
+      position: {
+        x: enemyPositionGenerator(),
+        y: 0,
+        z: -35,
+      },
+      velocity: { x: 0, y: 0, z: enemyVelocity },
+      color: randomColor,
+      zAcceleration: true,
+    });
+
+    const geometries = [
+      new THREE.SphereGeometry(0.5, 32, 32),
+      new THREE.ConeGeometry(0.5, 1, 32),
+      new THREE.BoxGeometry(1, 1, 1),
+    ];
+    const randomGeometry =
+      geometries[Math.floor(Math.random() * geometries.length)];
+    enemy.geometry = randomGeometry;
+
+    enemy.castShadow = true;
+    scene.add(enemy);
+    enemies.push(enemy);
+  }
+
+  frames++;
+  controls.update();
+  composer.render();
+}
+animate();
+
+const audioElement = new Audio("../public/audio/music.mp3");
+audioElement.loop = true;
+
+let audioContext;
+let audioSource;
+let lowPassFilter;
+let gainNode;
+
+function setupAudioContext() {
+  if (!audioContext) {
+    audioContext = new (window.AudioContext || window.webkitAudioContext)();
+    audioSource = audioContext.createMediaElementSource(audioElement);
+
+    lowPassFilter = audioContext.createBiquadFilter();
+    lowPassFilter.type = "lowpass";
+    lowPassFilter.frequency.setValueAtTime(20000, audioContext.currentTime);
+
+    gainNode = audioContext.createGain();
+    gainNode.gain.setValueAtTime(1, audioContext.currentTime);
+
+    audioSource.connect(gainNode);
+    gainNode.connect(lowPassFilter);
+    lowPassFilter.connect(audioContext.destination);
+  }
+}
+
+function toggleAudioPlayPause() {
+  if (audioContext && audioContext.state === "suspended") {
+    audioContext.resume();
+  }
+
+  if (audioElement.paused) {
+    audioElement
+      .play()
+      .then(() => {
+        document.getElementById("audio-img").src =
+          "https://cdn4.iconfinder.com/data/icons/mosaicon-05/512/nosound-512.png";
+        console.log("Audio started playing.");
+      })
+      .catch((error) => console.warn("Autoplay blocked:", error));
+  } else {
+    audioElement.pause();
+    document.getElementById("audio-img").src =
+      "https://cdn-icons-png.flaticon.com/512/786/786373.png";
+  }
+}
+
+function muffleAudio() {
+  if (lowPassFilter && gainNode) {
+    lowPassFilter.frequency.setValueAtTime(400, audioContext.currentTime);
+    gainNode.gain.setValueAtTime(0.4, audioContext.currentTime);
+    console.log("Audio muffled.");
+  }
+}
+
+function unmuffleAudio() {
+  if (lowPassFilter && gainNode) {
+    lowPassFilter.frequency.setValueAtTime(20000, audioContext.currentTime);
+    gainNode.gain.setValueAtTime(1, audioContext.currentTime);
+    console.log("Audio unmuffled.");
+  }
+}
+
+document
+  .getElementById("audio-play-pause-btn")
+  .addEventListener("click", () => {
+    setupAudioContext();
+    toggleAudioPlayPause();
+    document.getElementById("audio-play-pause-btn").blur();
+  });
+
+window.addEventListener("keydown", (event) => {
+  setupAudioContext();
+
+  if (event.code === "KeyM") {
+    toggleAudioPlayPause();
+    document.activeElement.blur();
+  }
+
+  if (event.code === "KeyG") {
+    muffleAudio();
+  }
+
+  if (event.code === "KeyU") {
+    unmuffleAudio();
+  }
+});
+
+document.addEventListener("visibilitychange", () => {
+  if (document.visibilityState === "hidden") {
+    if (!isPaused && !isGameOver) {
+      pauseGame();
+    }
+  } else if (document.visibilityState === "visible") {
+    if (!isPaused && !isGameOver) {
+      resumeGame();
+    }
+  }
+});
---
+++ src/index.js
@@ -0,0 +1,225 @@
+import * as THREE from "three";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  set,
+  get,
+} from "https://www.gstatic.com/firebasejs/11.0.2/firebase-database.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBDP807efNTMOuQc6jdi23NKnb2LbvxBlM",
+  authDomain: "infinite-horizon-1.firebaseapp.com",
+  projectId: "infinite-horizon-1",
+  storageBucket: "infinite-horizon-1.firebasestorage.app",
+  messagingSenderId: "393334686227",
+  appId: "1:393334686227:web:06ca63ecd18e2c43d2f5e6",
+  measurementId: "G-GDXH9NE5PT",
+};
+
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+
+document
+  .getElementById("leaderboard-btn")
+  .addEventListener("click", function () {
+    const leaderboard = document.getElementById("leaderboard");
+    leaderboard.style.display =
+      leaderboard.style.display === "none" || leaderboard.style.display === ""
+        ? "block"
+        : "none";
+    document.getElementById("leaderboard-btn").textContent =
+      leaderboard.style.display === "none"
+        ? "Show Leaderboard"
+        : "Hide Leaderboard";
+  });
+
+document
+  .getElementById("close-leaderboard")
+  .addEventListener("click", function () {
+    document.getElementById("leaderboard").style.display = "none";
+    document.getElementById("leaderboard-btn").textContent = "Show Leaderboard";
+  });
+
+function fetchLeaderboard() {
+  const leaderboardRef = ref(database, "players");
+  get(leaderboardRef)
+    .then((snapshot) => {
+      if (snapshot.exists()) {
+        const players = snapshot.val();
+        const leaderboard = Object.keys(players)
+          .map((key) => ({
+            name: players[key].name,
+            score: players[key].score,
+          }))
+          .sort((a, b) => b.score - a.score);
+
+        displayLeaderboard(leaderboard);
+      } else {
+        console.log("No players found");
+      }
+    })
+    .catch((error) => {
+      console.error("Error fetching leaderboard: ", error);
+    });
+}
+
+function displayLeaderboard(leaderboard) {
+  const leaderboardList = document.getElementById("leaderboard-list");
+  leaderboardList.innerHTML = "";
+
+  leaderboard.forEach((player, index) => {
+    const playerElement = document.createElement("div");
+    playerElement.textContent = `${index + 1}. ${player.name}: ${player.score}`;
+    leaderboardList.appendChild(playerElement);
+  });
+}
+
+document.addEventListener("DOMContentLoaded", fetchLeaderboard);
+
+const toggleControlsBtn = document.getElementById("toggle-controls-btn");
+const controlsPanel = document.getElementById("controls-panel");
+
+toggleControlsBtn.addEventListener("click", () => {
+  if (
+    controlsPanel.style.display === "none" ||
+    controlsPanel.style.display === ""
+  ) {
+    controlsPanel.style.display = "block";
+    toggleControlsBtn.innerText = "Hide Controls";
+  } else {
+    controlsPanel.style.display = "none";
+    toggleControlsBtn.innerText = "Show Controls";
+  }
+});
+
+const form = document.getElementById("send-it");
+
+document.getElementById("submit-name").addEventListener("click", () => {
+  const name = document.getElementById("name").value;
+  if (name) {
+    const userRef = ref(database, "players/" + name);
+    set(userRef, {
+      name: name,
+    })
+      .then(() => {
+        window.location.href = "game.html";
+      })
+      .catch((error) => {
+        console.error("Error saving name: ", error);
+      });
+  } else {
+    alert("Please enter your name!");
+  }
+});
+
+const canvas = document.getElementById("c");
+const hiddens = document.querySelectorAll(".hidden");
+
+document.addEventListener("DOMContentLoaded", () => {
+  canvas.style.opacity = 1;
+  hiddens.forEach((hid) => hid.classList.remove("hidden"));
+});
+
+const getRandomParticelPos = (particleCount) => {
+  const arr = new Float32Array(particleCount * 3);
+  for (let i = 0; i < particleCount; i++) {
+    arr[i] = (Math.random() - 0.5) * 10;
+  }
+  return arr;
+};
+const resizeRendererToDisplaySize = (renderer) => {
+  const canvas = renderer.domElement;
+  const width = canvas.clientWidth;
+  const height = canvas.clientHeight;
+  const needResize = canvas.width !== width || canvas.height !== height;
+  if (needResize) {
+    renderer.setSize(width, height, false);
+  }
+  return needResize;
+};
+
+let mouseX = 0;
+let mouseY = 0;
+document.addEventListener("mousemove", (e) => {
+  mouseX = e.clientX;
+  mouseY = e.clientY;
+});
+
+const starFieldBG = () => {
+  const canvas = document.getElementById("c");
+  const renderer = new THREE.WebGLRenderer({ canvas });
+  renderer.setClearColor(new THREE.Color("#000000"));
+  const scene = new THREE.Scene();
+
+  const color = 0xffffff,
+    intensity = 1;
+  const light = new THREE.DirectionalLight(color, intensity);
+  light.position.set(-1, 2, 4);
+  scene.add(light);
+
+  const fov = 75,
+    aspect = 2,
+    near = 1.5,
+    far = 5;
+  const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+  camera.position.z = 2;
+
+  const geometrys = [new THREE.BufferGeometry(), new THREE.BufferGeometry()];
+
+  geometrys[0].setAttribute(
+    "position",
+    new THREE.BufferAttribute(getRandomParticelPos(350), 3)
+  );
+  geometrys[1].setAttribute(
+    "position",
+    new THREE.BufferAttribute(getRandomParticelPos(1500), 3)
+  );
+
+  const loader = new THREE.TextureLoader();
+
+  const materials = [
+    new THREE.PointsMaterial({
+      size: 0.05,
+      map: loader.load(
+        "https://raw.githubusercontent.com/Kuntal-Das/textures/main/sp1.png"
+      ),
+      transparent: true,
+      color: "#0369a1",
+    }),
+    new THREE.PointsMaterial({
+      size: 0.075,
+      map: loader.load(
+        "https://raw.githubusercontent.com/Kuntal-Das/textures/main/sp2.png"
+      ),
+      transparent: true,
+      // color: "#0369a1"
+    }),
+  ];
+
+  const starsT1 = new THREE.Points(geometrys[0], materials[0]);
+  const starsT2 = new THREE.Points(geometrys[1], materials[1]);
+  scene.add(starsT1);
+  scene.add(starsT2);
+
+  const render = (time) => {
+    time *= 0.001;
+
+    if (resizeRendererToDisplaySize(renderer)) {
+      const canvas = renderer.domElement;
+      camera.aspect = canvas.clientWidth / canvas.clientHeight;
+      camera.updateProjectionMatrix();
+    }
+
+    starsT1.position.x = mouseX * 0.0001;
+    starsT1.position.y = mouseY * -0.0001;
+
+    starsT2.position.x = mouseX * 0.0001;
+    starsT2.position.y = mouseY * -0.0001;
+
+    renderer.render(scene, camera);
+    requestAnimationFrame(render);
+  };
+  requestAnimationFrame(render);
+};
+starFieldBG();
